45853d236d6cd54710caa0f3a09d45fb85dfa194
Rahul Gupta <rg5353070@gmail.com>
2024-02-27 23:43:51 +0530

    services and capabilities

--- slices/Capabilities/index.tsx
+++ slices/Capabilities/index.tsx
@@ -45,7 +45,7 @@ const Capabilities = ({ slice }: CapabilitiesProps): JSX.Element => {
         </div>
         <div className="mt-6 grid grid-cols-1 gap-6 rounded-xl px-6 py-8 shadow-md md:mt-12 md:grid-cols-2">
           <div>
-            <h2>Services and Capabilities</h2>
+            <h2>Capabilities</h2>
             <span className="mt-4 block font-semibold text-gray-600">
               Beyond the horizon of traditional investment, Dark Alpha shines a
               light on hidden potential. We're an independent sponsor startup
@@ -106,7 +106,7 @@ const Capabilities = ({ slice }: CapabilitiesProps): JSX.Element => {
               className="mt-4 inline-block border-2 px-6 py-2 font-semibold text-[#7a9e9f] transition hover:bg-[#7a9e9f] hover:text-white"
               href="/contact"
             >
-              Contact Us
+              CONTACT US
             </Link>
           </div>
         </div>
