9b0005fc2bb376c04985a672d490b18038f1aa7c
Rahul Gupta <rg5353070@gmail.com>
2024-03-06 21:30:05 +0530

    added a portfolio hero website

--- slices/TextWithBackground/index.tsx
+++ slices/TextWithBackground/index.tsx
@@ -3,6 +3,9 @@ import TextBackgroundImage from "@/components/TextBackgroundImage";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import Link from "next/link";
+import HeroLogo from "@/public/heroTechLogo.png";
+import Image from "next/image";
+import { MdArrowOutward } from "react-icons/md";
 
 /**
  * Props for `TextWithBackground`.
@@ -21,25 +24,28 @@ const TextWithBackground = ({
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       backgroundImage={slice.primary.backgroundimage}
-      classname="min-h-[40vh]"
+      classname="min-h-[70vh] "
     >
-      <div className="big-container block-space">
-        <div className="grid grid-cols-1 md:grid-cols-2">
-          <div className="">
-            <h2 className="text-primary">{slice.primary.heading}</h2>
-            <span className="mt-2 block text-primary">
-              {slice.primary.tagline}
-            </span>
-          </div>
-          <div className="">
-            {/* <div className="mt-4 transition hover:scale-105">
-              <Link
-                className="text-uppercase btn-primary px-6 py-2 font-bold text-white"
-                href="/contact"
-              >
-                Set an Appointment
-              </Link>
-            </div> */}
+      <div className="big-container block-space text-primary">
+        <div className="text-center">
+          <h2 className="text-primary">{slice.primary.heading}</h2>
+          <h4 className="mt-2 block ">{slice.primary.tagline}</h4>
+        </div>
+
+        <div className="mt-6 flex">
+          <div className="text-center">
+            <Image
+              src={HeroLogo}
+              alt="logo for dark alpha's portfolio company Hero Technologies"
+            />
+            <h4>Hero Technologies</h4>
+            <Link
+              href={"https://herotechnologiesinc.com/"}
+              target="_blank"
+              className="flex items-center justify-center text-yellow-500"
+            >
+              Visit Site <MdArrowOutward />
+            </Link>
           </div>
         </div>
       </div>
